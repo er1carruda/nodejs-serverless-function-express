db5e370ecf5b5a27f73b6b2710f54fa69b12e4f3
Éric Arruda <ericarrudaa363@gmail.com>
2024-12-20 15:10:27 -0300

    Corrigir arquivo hello.js

---
+++ api/hello.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  const query = req.query.query || 'Explain how AI works';
+
+  const body = {
+    "contents": [{
+      "parts": [{"text": query}]
+    }]
+  };
+
+  const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyD1n_eWixzYfOr5pbCuAx88D_tI1j-DLu0`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+
+  const data = await response.json();
+  const answer = data?.contents?.[0]?.parts?.[0]?.text || 'Não foi possível obter uma resposta.';
+
+  res.status(200).send(answer);
+}
